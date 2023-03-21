f98bd0a8eb2b8c1427af9ec7f5b598e58386a8a3
ldavies99 <Luke.Davies@ext.ons.gov.uk>
2023-03-21 15:19:03 +0000

    Add sub theme and main theme matching functions

---
+++ collate_sheets.R
@@ -0,0 +1,36 @@
+
+# TODO: Write function documentation
+
+read_sheets <- function(sheet_name, config) {
+  
+  files <- list.files(config$input_path)
+  
+  files <- paste0(config$input_path, "/",  files)
+  
+  files <- files[-grep("~", files)]
+  
+  all_tables <- lapply(files, function(file) {
+    data <- openxlsx::read.xlsx(file, sheet = sheet_name)
+    division_info <- openxlsx::read.xlsx(file, sheet = "Division_info")
+    
+    # Drop empty data sets because some divisions have no actions
+    if (nrow(data) > 0) {
+      
+      data %<>% 
+        dplyr::mutate(divsion = colnames(division_info)[2], .before = everything(),
+                      dplyr::across(dplyr::everything(), as.character))
+      
+      return(data)
+    }
+    
+  })
+  
+  do.call(dplyr::bind_rows, all_tables)
+  
+}
+
+save_sheet <- function(config) {
+  for (sheet_name in config$sheet_names) {
+    write.csv(read_sheets(sheet_name, config), paste0(config$output_path, "/", sheet_name, ".csv"))
+  }
+}
--- main.R
+++ main.R
@@ -2,38 +2,44 @@ library(yaml)
 library(xlsx)
 library(magrittr)
 library(dplyr)
+source("collate_sheets.R")
 
 config <- yaml::read_yaml("config.yml")
 
+dummy_data <- data.frame("col1" = c("a", "e", "c"),
+                         "col2" = c("d", "a", "f"),
+                         "col3" = c("a", "d", "c"))
 
-read_sheets <- function(sheet_name, config) {
-  
-  files <- list.files(config$input_path)
-  
-  files <- paste0(config$input_path, "/",  files)
-  
-  files <- files[-grep("~", files)]
-  
-  all_tables <- lapply(files, function(file) {
-    data <- openxlsx::read.xlsx(file, sheet = sheet_name)
-    division_info <- openxlsx::read.xlsx(file, sheet = "Division_info")
-    
-    # Drop empty data sets because some divisions have no actions
-    if (nrow(data) > 0) {
-      
-      data %<>% 
-        dplyr::mutate(divsion = colnames(division_info)[2], .before = everything(),
-                      dplyr::across(dplyr::everything(), as.character))
+theme = list(sub1 = c("a", "b", "c"), sub2 = c("d", "e", "f"))
 
-      return(data)
-    }
-    
-  })
-  
-  do.call(dplyr::bind_rows, all_tables)
+
+match_theme <- function() {
   
 }
 
-for (sheet_name in config$sheet_names) {
-  write.csv(read_sheets(sheet_name, config), paste0(config$output_path, "/", sheet_name, ".csv"))
+match_theme <- function(data, theme_dict){
+  
+  apply(data, 1, function(row){
+    
+    sapply(theme_dict, function(theme){
+      
+      any(row[theme])
+      
+    })
+    
+  }) %>% t %>% data.frame
+  
+}
+
+
+match_sub_theme <- function(col, theme_dict) {
+
+  sapply(theme_dict, function(theme) {
+    
+    theme <- paste0(theme, collapse = "|")
+    
+    grepl(pattern = theme, x = col)
+    
+  }) %>% data.frame
+  
 }
