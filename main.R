926ae55decf82828c733e28c40e8698880888344
shilohd <daniel.shiloh@ons.gov.uk>
2023-03-21 12:21:06 +0000

    read file function

--- main.R
+++ main.R
@@ -1,3 +1,35 @@
 library(yaml)
+library(magrittr)
 
-yaml::read_yaml("themes.yaml")
+config <- yaml::read_yaml("config.yml")
+
+
+read_sheets <- function(sheet_name, config) {
+  
+  files <- list.files(config$input_path)
+  
+  files <- paste0(config$input_path, "/",  files)
+  
+  # TODO: remove filenames with "~"
+  
+  all_tables <- lapply(files, function(file) {
+    data <- openxlsx::read.xlsx(file, sheet = sheet_name)
+    division_info <- openxlsx::read.xlsx(file, sheet = "Division_info")
+    
+    # TODO: deal with empty sheets
+    
+    data$division <- colnames(division_info)[2]
+    
+    data %<>% 
+      dplyr::mutate(dplyr::across(dplyr::everything(), as.character))
+    
+    # TODO: reorder data so that division name is the first column
+    
+    return(data)
+  })
+  
+  do.call(dplyr::bind_rows, all_tables)
+  
+}
+
+# TODO: load all relevant sheets and save to output folder (outputs/) - folder name should come from config file. Save as CSV
