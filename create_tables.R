0ebeae48b6754355f752c5a34f7a454d6e83f957
Snook <ruth.snook@ons.gov.uk>
2023-03-27 16:26:16 +0100

    add func to filer data on ownership bucket and correct colnames.

--- create_tables.R
+++ create_tables.R
@@ -3,18 +3,26 @@ create_divisional_tables <- function(data) {
   for (owner in names(config$support_owner$owners)){
     output[[owner]] <- data %>% 
       dplyr::filter(!!as.name(owner)) %>% 
-      dplyr::select(division, 
-                    "risk_title" = title.x, 
-                    "risk_description" = description,
-                    action, 
-                    definition_of_done,
+      dplyr::select("Division" = division, 
+                    "Risk title" = title.x, 
+                    "Risk description" = description,
+                    "Action" = action, 
+                    "Action definition of Done" = definition_of_done,
                     ownership_bucket_imputed) %>% 
-      dplyr::mutate("will_action_be_delivered_in_financial_year" = NA,
-                    "comments" = NA,
-                    "must_have_or_funding_dependent" = NA,
-                    "impact_if_not_delivered" = NA,
-                    "completed_by" = NA)
+      dplyr::mutate("Will this action be delivered under the planning based on provisional allocated funding for the next financial year? Y or N" = NA,
+                    "Any comments on the action" = NA,
+                    "Is this action part of your must-have plans or is it dependent on the final funding allocaton?" = NA,
+                    "If the action is not delivered what would be the impact on the risk to quality?" = NA,
+                    "Completed by" = NA)
   }
   return(output)
 }
 
+format_owner_table <- function(data){
+  output <- data %>% 
+    dplyr::filter(ownership_bucket_imputed %in%
+                    c("2. Control - With support", "3. Outside control")) %>% 
+    dplyr::select(-ownership_bucket_imputed)
+    
+  return(output)
+}
